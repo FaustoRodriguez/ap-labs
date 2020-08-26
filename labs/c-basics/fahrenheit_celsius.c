c2eeda27d6d7578ac882c744cafc467016f96e72
José Fausto Rodriguez <a01634943@itesm.mx>
2020-08-26 00:31:17 +0000

    c-basics2
    Signed-off-by: José Fausto Rodriguez <a01634943@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
-#define   LOWER  0       /* lower limit of table */
-#define   UPPER  300     /* upper limit */
-#define   STEP   20      /* step size */
 
-/* print Fahrenheit-Celsius table */
+void convertFtoC(int f){
+    printf("Fahrenheit: %3d, Celcius: %6.1f\n", f, (5.0/9.0)*(f-32));
+}
 
-int main()
+int main(int argc, char **argv)
 {
-    int fahr;
+    int start,
+        end,
+        increment;
+    if(argc>2){
+        start   =   atoi(argv[1]);
+        end   =   atoi(argv[2]);
+        increment   =   atoi(argv[3]);
+        for (start; start <= end; start = start + increment)
+            convertFtoC(start);
+    }
+    else{
+        if (argc    ==  0)
+        {
+            printf("Values were not introduced");
+            return 0;
+        }
+        convertFtoC(atoi(argv[1]));
 
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-	printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
-	printf("Hello world");
+    }
     return 0;
 }
