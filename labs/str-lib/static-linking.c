3dbb481f455ac710d02f4efafbd50e1636295eda
José Fausto Rodriguez <a01634943@itesm.mx>
2020-09-09 04:57:01 +0000

    str-lib
    Signed-off-by: José Fausto Rodriguez <a01634943@itesm.mx>

--- labs/str-lib/static-linking.c
+++ labs/str-lib/static-linking.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    return 0;
+int main(int argc, char **argv) {
+    if(argv[1]  ==  "-add"){
+        char    *added  =mystradd(argv[2],argv[3]);
+        printf("Initial Lenght      : %i\nNew String          : %s\nNew length          : %i",mystrlen(argv[2]),added,mystrlen(added));
+    }
+    else
+    {
+        if(argv[1]  ==  "-find"){
+            int pos =   mystrfind(argv[2]);
+            if(pos  !=  -1)
+                printf("['%s'] string was found at [%i] position",argv[2],pos);
+            else
+            {
+                printf("Substring not found");
+            }
+            
+        }
+    }
+    
 }
--- labs/str-lib/strlib.c
+++ labs/str-lib/strlib.c
@@ -1,13 +1,38 @@
 
-
 int mystrlen(char *str){
-    return 0;
+    int i;
+    for (i = 0; str[i] != '\0'; ++i);
+    return i;
 }
 
 char *mystradd(char *origin, char *addition){
-    return 0;
+    int lenOrigin   =   mystrlen(origin),
+        lenAddition =   mystrlen(addition),
+        i;
+    char    str[lenOrigin+lenAddition];
+    for (i  =   0; i    <   lenOrigin; i++)
+    {
+        str[i]  =   origin[i];
+    }
+    for (; i    <   lenOrigin+lenAddition; i++)
+    {
+        str[i]  =   addition[i   -   lenOrigin];
+    }
+    return str;
 }
 
 int mystrfind(char *origin, char *substr){
-    return 0;
+    int lenOrigin   =   mystrlen(origin),
+        lenSubstr   =   mystrlen(substr)-1,
+        i,
+        j;
+
+    for(i   =   0;  i   +   lenSubstr   <   lenOrigin;  i++){
+        for (j  =   0; j    <   lenSubstr   &&  origin[i  +   j]  ==   substr[j]; j++);
+        if(j    ==   lenSubstr){
+            return i;
+        } 
+    }
+
+    return -1;
 }
