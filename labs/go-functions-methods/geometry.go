4eab5aeeaafce33aacdf24180b9b7463fe66e1a4
José Fausto Rodriguez <a01634943@itesm.mx>
2020-09-23 04:51:36 +0000

    go-functions-methods

--- labs/go-functions-methods/geometry.go
+++ labs/go-functions-methods/geometry.go
@@ -7,18 +7,26 @@
 //!+point
 package main
 
-import "math"
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
 
-type Point struct{ X, Y float64 }
+//Point defined as a structure with two float values
+type Point struct{ x, y float64 }
 
-// traditional function
+//Distance traditional function
 func Distance(p, q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
 }
 
-// same thing, but as a method of the Point type
+//Distance same thing, but as a method of the Point type
 func (p Point) Distance(q Point) float64 {
-	return math.Hypot(q.X-p.X, q.Y-p.Y)
+	return math.Hypot(q.x-p.x, q.y-p.y)
 }
 
 //!-point
@@ -39,7 +47,103 @@ func (path Path) Distance() float64 {
 	return sum
 }
 
+//X returns y value of Point
+func (p Point) X() float64 {
+	return p.x
+}
+
+//Y returns y value of Point
+func (p Point) Y() float64 {
+	return p.y
+}
+func isSegment(p, q, r Point) bool {
+	if (q.X() <= math.Max(p.X(), r.X())) &&
+		(q.X() >= math.Min(p.X(), r.X())) &&
+		(q.Y() <= math.Max(q.Y(), r.Y())) &&
+		(q.Y() >= math.Min(p.Y(), r.Y())) {
+		return true
+	}
+	return false
+}
+
+func direction(p, q, r Point) int {
+	dir := (q.Y()-p.Y())*(r.X()-q.X()) - (q.X()-p.X())*(r.Y()-q.Y())
+	if dir == 0 {
+		return 0
+	}
+	if dir > 0 {
+		return 1
+	}
+	return 2
+}
+
+func (path Path) intersection() bool {
+	if len(path) == 3 {
+		return false
+	}
+	itdoes := false
+	for i := 0; i < len(path)-3; i++ {
+		for j := 0; j < i+1; j++ {
+			a := direction(path[j], path[j+1], path[len(path)-2])
+			b := direction(path[j], path[j+1], path[len(path)-1])
+			c := direction(path[len(path)-2], path[len(path)-1], path[j])
+			d := direction(path[len(path)-2], path[len(path)-1], path[j+1])
+			itdoes = false
+			if ((a != b) && (c != d)) ||
+				((a == 0) && isSegment(path[j], path[len(path)-2], path[j+1])) ||
+				((b == 0) && isSegment(path[j], path[len(path)-1], path[j+1])) ||
+				((c == 0) && isSegment(path[len(path)-2], path[j], path[len(path)-1])) ||
+				((d == 0) && isSegment(path[len(path)-2], path[j+1], path[len(path)-1])) {
+				itdoes = true
+			}
+		}
+	}
+	return itdoes
+}
+
+func random() float64 {
+	seed := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(seed)
+	return math.Round(((random.Float64()*200)-100)*100) / 100
+}
+
+func createFigure(sides int) Path {
+	figure := Path{}
+	for i := 0; i < sides; i++ {
+		figure = append(figure, Point{random(), random()})
+		for figure.intersection() {
+			figure[i] = Point{random(), random()}
+		}
+	}
+	return figure
+}
+
 func main() {
+	if len(os.Args) > 0 {
+		sides, _ := strconv.Atoi(os.Args[1])
+		if sides < 3 {
+			fmt.Println("WRONG INPUT: Number of sides must be over 3 to make a figure")
+			return
+		}
+		figure := createFigure(sides)
+		fmt.Printf("- Generating a [%d] sides figure\n- Figure's vertices\n", sides)
+		for i := 0; i < sides; i++ {
+			fmt.Printf("\t- ( %.1f, %.1f)\n", figure[i].X(), figure[i].Y())
+		}
+		fmt.Println("- Figure's Perimeter")
+		var per = 0.0
+		fmt.Print("\t- ")
+		i := 0
+		for ; i < sides-1; i++ {
+			per = per + figure[i].Distance(figure[(i+1)%sides])
+			fmt.Printf("%.2f + ", figure[i].Distance(figure[(i+1)%sides]))
+		}
+		per = per + figure[i].Distance(figure[(i)%sides])
+		fmt.Printf("%.2f = %.2f\n", figure[i].Distance(figure[(i)%sides]), per)
+	} else {
+		fmt.Println("No argument introduced")
+	}
+	return
 }
 
 //!-path
