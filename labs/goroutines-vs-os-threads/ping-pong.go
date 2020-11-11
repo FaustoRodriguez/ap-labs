8483161906ffadaeda5ab5872fb62830f6f64297
José Fausto Rodriguez <a01634943@itesm.mx>
2020-11-11 05:57:15 +0000

    goroutines-vs-os-threads

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,28 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 	var commsPerSecond int
+	commsPerSecond = 0
+	ping := make(chan int)
+	pong := make(chan int)
+	go play(ping, pong)
+	go play(pong, ping)
+
+	for i := 0; i < 1; i++ {
+		ping <- 0
+		time.Sleep(time.Duration(1) * time.Second)
+		commsPerSecond += <-ping
+	}
 
 	fmt.Println("Communications Per Second : ", commsPerSecond)
+}
+
+func play(ping chan int, pong chan int) {
+	for {
+		pong <- (1 + <-ping)
+	}
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var maxStages, transitTime int
-
+	var maxStages int
+	nPipelines := 100000
+	first := newPipeline()
+	last := first
+	for i := 0; i < nPipelines; i++ {
+		maxStages++
+		previous := last
+		newLast := newPipeline()
+		go transfer(previous, newLast)
+		last = newLast
+	}
+	startTime := time.Now()
+	first <- 1
+	<-last
+	finnishTime := time.Now()
+	transitTime := finnishTime.Sub(startTime)
 	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
 	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+}
+
+func newPipeline() chan int {
+	return make(chan int)
+}
+
+func transfer(previous chan int, newLast chan int) {
+	for i := range previous {
+		newLast <- i
+	}
 }
